cf9203bac7530b2b4f473fb4323469537e5e90c4
BaekGeunYoung <dvmflstm@naver.com>
2020-04-20 18:22:36 +0900

    컨피그 서버 새로 생성

---
+++ config/src/main/kotlin/com/project/zzappang/config/ConfigApplication.kt
@@ -0,0 +1,11 @@
+package com.project.zzappang.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigApplication>(*args)
+}

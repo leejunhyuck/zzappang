fb2678403dbec1962e6523e9361562a9f22e53c6
BaekGeunYoung <dvmflstm@naver.com>
2020-04-20 20:18:37 +0900

    gateway : @EnableZuulProxy 적용 및 build.gradle에서 spring cloud 모듈의 버전을 자동 적용할 수 있도록 설정

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -15,13 +15,24 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "Hoxton.SR3"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
 }
 
--- gateway/src/main/kotlin/com/project/zzappang/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/com/project/zzappang/gateway/GatewayApplication.kt
@@ -2,8 +2,10 @@ package com.project.zzappang.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableZuulProxy
 class GatewayApplication
 
 fun main(args: Array<String>) {

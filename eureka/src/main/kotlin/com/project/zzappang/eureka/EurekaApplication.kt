2c3fbed5125dba111079cc6b1062d552b57d4e45
BaekGeunYoung <dvmflstm@naver.com>
2020-04-20 20:17:52 +0900

    eureka server : 기본적인 application.yml 작성 및 @EnableEurekaServer 적용

--- eureka/src/main/kotlin/com/project/zzappang/eureka/EurekaApplication.kt
+++ eureka/src/main/kotlin/com/project/zzappang/eureka/EurekaApplication.kt
@@ -2,8 +2,10 @@ package com.project.zzappang.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaApplication
 
 fun main(args: Array<String>) {

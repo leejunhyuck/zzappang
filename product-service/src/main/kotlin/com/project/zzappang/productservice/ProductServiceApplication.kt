75802ae9c56b24455c8594ac94942b95653ecdc1
BaekGeunYoung <dvmflstm@naver.com>
2020-04-21 22:35:16 +0900

    product-service 생성

---
+++ product-service/src/main/kotlin/com/project/zzappang/productservice/ProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.project.zzappang.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductServiceApplication>(*args)
+}

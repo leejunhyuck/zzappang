b88cc80d532cfd71e6a3902280e852d4fe14fd00
BaekGeunYoung <dvmflstm@naver.com>
2020-04-21 19:58:08 +0900

    gateway : spring cloud bus 사용을 위한 dependency 및 설정값 추가

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }

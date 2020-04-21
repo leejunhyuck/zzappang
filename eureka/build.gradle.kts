6fa4574a1dc88b7ae98611a1ccceaa13de664cbf
BaekGeunYoung <dvmflstm@naver.com>
2020-04-21 19:57:54 +0900

    eureka server : spring cloud bus 사용을 위한 dependency 및 설정값 추가

--- eureka/build.gradle.kts
+++ eureka/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }

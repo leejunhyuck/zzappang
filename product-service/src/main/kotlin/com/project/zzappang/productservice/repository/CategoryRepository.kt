31ab3bc648f01222d87d069fd6dccaf8cc2395c3
BaekGeunYoung <dvmflstm@naver.com>
2020-04-22 18:54:01 +0900

    ReactiveCrudRepository를 이용해 Category와 Product에 대한 repository 클래스 생성

--- product-service/src/main/kotlin/com/project/zzappang/productservice/repository/CategoryRepository.kt
+++ product-service/src/main/kotlin/com/project/zzappang/productservice/repository/CategoryRepository.kt
@@ -1,2 +1,6 @@
 package com.project.zzappang.productservice.repository
 
+import com.project.zzappang.productservice.domain.Category
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+
+interface CategoryRepository : ReactiveCrudRepository<Category, String>
--- product-service/src/main/kotlin/com/project/zzappang/productservice/repository/ProductRepository.kt
+++ product-service/src/main/kotlin/com/project/zzappang/productservice/repository/ProductRepository.kt
@@ -1,2 +1,6 @@
 package com.project.zzappang.productservice.repository
 
+import com.project.zzappang.productservice.domain.Product
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+
+interface ProductRepository : ReactiveCrudRepository<Product, String>

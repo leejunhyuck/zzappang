671058a78be2903ba502fe5cce2ed2362fba00bb
BaekGeunYoung <dvmflstm@naver.com>
2020-04-25 16:00:01 +0900

    product-service : categoryRepository에 findByNameContaining, findByParentCategoryId 함수 추가

--- product-service/src/main/kotlin/com/project/zzappang/productservice/repository/CategoryRepository.kt
+++ product-service/src/main/kotlin/com/project/zzappang/productservice/repository/CategoryRepository.kt
@@ -2,5 +2,9 @@ package com.project.zzappang.productservice.repository
 
 import com.project.zzappang.productservice.domain.Category
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 
-interface CategoryRepository : ReactiveCrudRepository<Category, String>
+interface CategoryRepository : ReactiveCrudRepository<Category, String> {
+    fun findByNameContaining(name: String): Flux<Category>
+    fun findByParentCategoryId(parentCategoryId: String): Flux<Category>
+}

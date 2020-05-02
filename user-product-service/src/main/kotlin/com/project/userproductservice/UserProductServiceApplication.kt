618ed4844b61e0d0391874dd004a3f5d872d561c
Leejunhyuck <hg00521@naver.com>
2020-05-02 10:17:00 +0900

    user-product-service 브랜치 및 프로젝트 생성

---
+++ user-product-service/src/main/kotlin/com/project/userproductservice/UserProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.project.userproductservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserProductServiceApplication>(*args)
+}
---
+++ user-product-service/src/test/kotlin/com/project/userproductservice/UserProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.userproductservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserProductServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
